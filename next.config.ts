b4a20e1404cedca3aa2b4c62428fec344711faee
kalijerry <729913310@qq.com>
2025-06-16 21:16:03 +0800

    更新本地打包 /out 用来构建跨平台应用

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    // 核心改动：只有在环境变量为 'true' 时才启用 'export' 模式
+    output: process.env.IS_STATIC_EXPORT === 'true' ? 'export' : undefined,
 };
 
 export default nextConfig;
